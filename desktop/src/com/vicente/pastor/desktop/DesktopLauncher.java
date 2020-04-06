350f96df42dba7d606c5370638be774cc97fd94d
vpasbar <vicentepastor@microedet.com>
2020-04-06 19:04:06 +0200

    Initial commit

---
+++ desktop/src/com/vicente/pastor/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.vicente.pastor.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.vicente.pastor.MainScreen;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainScreen(), config);
+	}
+}
